caad0accef887d6005bbe520e79f1a7651058c82
Chris K.Y. FUNG <8746768+chriskyfung@users.noreply.github.com>
2024-01-19 01:33:00 +0800

    🔨update(gulp): remove css2scc() and minifyCSS()
    This commit removes the unused gulp tasks and their depending packages,
    i.e. gulp-csso, gulp-ext-replace, and @gecka/styleflux, from the
    devDependencies for the project. Also, it fixes the vulnerabilities in
    the packages by running `npm audit fix`.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
 const {src, dest, watch, series, parallel} = require('gulp');
-const csso = require('gulp-csso');
-const ext_replace = require('gulp-ext-replace');
 const htmlmin = require('gulp-htmlmin');
 const minifyInline = require('gulp-minify-inline');
 const processIfModified = require('gulp-process-if-modified');
@@ -11,8 +9,6 @@ const AmpOptimizer = require('@ampproject/toolbox-optimizer');
 const ampOptimizer = AmpOptimizer.create();
 const gulpAmpValidator = require('gulp-amphtml-validator');
 const amphtmlValidator = require('amphtml-validator');
-
-const cssConverter = require('@gecka/styleflux');
 
 function build(cb) {
   return src('./_site/**/*.html')
@@ -69,39 +65,6 @@ function validate() {
     );
 }
 
-function css2scss() {
-  return src(['./_includes/css/*.css', '!**/*.min.css'])
-    .pipe(processIfModified())
-    .pipe(
-      through2.obj(async (file, _, cb) => {
-        if (file.isBuffer()) {
-          const scssify = await cssConverter.cssToScss(
-            file.contents.toString()
-          );
-          file.contents = Buffer.from(scssify);
-        }
-        cb(null, file);
-      })
-    )
-    .pipe(ext_replace('.scss'))
-    .pipe(dest('./_sass/'));
-}
-
-function minifyCSS() {
-  return src(['./_includes/css/*.css', '!**/*.min.css'])
-    .pipe(processIfModified())
-    .pipe(csso())
-    .pipe(ext_replace('.min.css'))
-    .pipe(dest('./_includes/css/'));
-}
-
-exports.default = function() {
-  watch(['./_includes/css/*.css', '!**/*.min.css'], parallel(css2scss, minifyCSS));
-};
-
 exports.build = build;
 exports.test = test;
-exports.convert = parallel(css2scss, minifyCSS);
-exports.css2scss = css2scss;
-exports.minifyCSS = minifyCSS;
 exports.validate = validate;
