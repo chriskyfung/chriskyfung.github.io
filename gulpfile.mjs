74c839b44c99e0732c0a9f1cf68930c5b31e0a6d
Chris K.Y. FUNG <8746768+chriskyfung@users.noreply.github.com>
2024-09-30 20:14:43 +0800

    jekyll build from Action 0094a9b1b35a5bda23d3a4b06c44d691c40e5a08

---
+++ gulpfile.mjs
@@ -0,0 +1,66 @@
+import { src, dest, watch, series, parallel } from 'gulp';
+import htmlmin from 'gulp-html-minifier-terser';
+import gulpAmpValidator from 'gulp-amphtml-validator';
+
+import through2 from 'through2';
+import amphtmlValidator from 'amphtml-validator';
+import AmpOptimizer from '@ampproject/toolbox-optimizer';
+
+const ampOptimizer = AmpOptimizer.create();
+
+function build(cb) {
+  return src('./_site/**/*.html')
+    .pipe(
+      through2.obj(async (file, _, cb) => {
+        if (file.isBuffer()) {
+          const date = new Date();
+          console.log(`[\x1b[90m${date.toLocaleTimeString('it-IT')}\x1b[0m] Running AMP Optimizer on ${file.path}`);
+          const optimizedHtml = await ampOptimizer.transformHtml(
+            file.contents.toString()
+          );
+          file.contents = Buffer.from(optimizedHtml);
+        }
+        cb(null, file);
+      })
+    )
+    .pipe(htmlmin({ collapseWhitespace: false }))
+    .pipe(dest('./_site/'));
+}
+
+function test() {
+  return src('./_site/**/*.html')
+      // Validate the input and attach the validation result to the "amp" property
+      // of the file object.
+      .pipe(gulpAmpValidator.validate())
+      // Print the validation results to the console.
+      .pipe(gulpAmpValidator.format())
+      // Exit the process with error code (1) if an AMP validation error
+      // occurred.
+      .pipe(gulpAmpValidator.failAfterWarningOrError());
+}
+
+function validate() {
+  return src('./_site/**/*.html')
+    .pipe(
+      through2.obj(async (file, _, cb) => {
+        if (file.isBuffer()) {
+          const validator = await amphtmlValidator.getInstance();
+          const result = validator.validateString(file.contents.toString());
+          if (result.status !== 'PASS') console.error(`\n${result.status}: ${file.relative}`);
+          // (result.status === 'PASS' ? console.log : console.error)(result.status);
+          for (var ii = 0; ii < result.errors.length; ii++) {
+            var error = result.errors[ii];
+            var msg =
+              'line ' + error.line + ', col ' + error.col + ': ' + error.message;
+            if (error.specUrl !== null) {
+              msg += ' (see ' + error.specUrl + ')';
+            }
+            (error.severity === 'ERROR' ? console.error : console.warn)(msg);
+          }     
+        }
+        cb(null, file);
+      })
+    );
+}
+
+export { build, test, validate };
